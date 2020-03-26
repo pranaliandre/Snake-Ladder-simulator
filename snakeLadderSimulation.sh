9d5534accd0c28c2845f2ea98d0e6a3f44f2f5d3
pranaliandre <pranaliandre06@gmail.com>
2020-03-26 10:30:06 -0500

    Added: function to check if its no play, snake and ladder

--- snakeLadderSimulation.sh
+++ snakeLadderSimulation.sh
@@ -1,7 +1,30 @@
-#!/bin/bash -x 
-echo "welcome to smake and ladder simulation"
+#!/bin/bash -x
 
+echo "welcome to smake and ladder simulation"
+#Constant Variable
+NO_PLAY=0
+LADDER=1
+SNAKE=2
 #variable
 positionPlayer=0
-#Player roll dir and to get a umbet 1 to 6
-rollDie=$(( RANDOM % 6 + 1))
+#function of  player then checks for a option NO_PLAY, SNAKE and LADDER
+function setPlayer(){
+	#Player roll die and to get a number 1 to 6
+	rollDie=$(( RANDOM % 6 + 1 ))
+	
+	playingOptions=$(( RANDOM % 3 ))
+	#Move player position according to playing options
+	case $playingOptions in
+		$NO_PLAY)
+			playingOptions=$playingOptions
+			;;
+		$LADDER)
+			playingOptions=$(( $playingOptions + $rollDie))
+			;;
+		$SNAKE)
+			playingOptions=$(( $playingOptions - $rollDie ))
+			;;
+	esac
+}
+#start game
+setPlayer
